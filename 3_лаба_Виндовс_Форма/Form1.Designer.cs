d6bfb62bf378fde0ff78e72e3b6d83dd17ea0820
Dmitry Mamonov <dmitry_mamonovv@mail.ru>
2024-04-21 13:48:28 +0300

    Добавьте файлы проекта.

---
+++ 3_лаба_Виндовс_Форма/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace _3_лаба_Виндовс_Форма
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxX = new System.Windows.Forms.TextBox();
+            this.textBoxY = new System.Windows.Forms.TextBox();
+            this.textBoxZ = new System.Windows.Forms.TextBox();
+            this.textBoxResult = new System.Windows.Forms.TextBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxX
+            // 
+            this.textBoxX.Location = new System.Drawing.Point(288, 46);
+            this.textBoxX.Name = "textBoxX";
+            this.textBoxX.Size = new System.Drawing.Size(100, 22);
+            this.textBoxX.TabIndex = 0;
+            this.textBoxX.Text = "X";
+            // 
+            // textBoxY
+            // 
+            this.textBoxY.Location = new System.Drawing.Point(287, 118);
+            this.textBoxY.Name = "textBoxY";
+            this.textBoxY.Size = new System.Drawing.Size(100, 22);
+            this.textBoxY.TabIndex = 1;
+            this.textBoxY.Text = "Y";
+            // 
+            // textBoxZ
+            // 
+            this.textBoxZ.Location = new System.Drawing.Point(294, 198);
+            this.textBoxZ.Name = "textBoxZ";
+            this.textBoxZ.Size = new System.Drawing.Size(100, 22);
+            this.textBoxZ.TabIndex = 2;
+            this.textBoxZ.Text = "Z";
+            // 
+            // textBoxResult
+            // 
+            this.textBoxResult.Location = new System.Drawing.Point(483, 48);
+            this.textBoxResult.Name = "textBoxResult";
+            this.textBoxResult.Size = new System.Drawing.Size(100, 22);
+            this.textBoxResult.TabIndex = 3;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(475, 135);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCalculate.TabIndex = 4;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.textBoxResult);
+            this.Controls.Add(this.textBoxZ);
+            this.Controls.Add(this.textBoxY);
+            this.Controls.Add(this.textBoxX);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxX;
+        private System.Windows.Forms.TextBox textBoxY;
+        private System.Windows.Forms.TextBox textBoxZ;
+        private System.Windows.Forms.TextBox textBoxResult;
+        private System.Windows.Forms.Button buttonCalculate;
+    }
+}
+
---
+++ 3_лаба_Виндовс_Форма/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _3_лаба_Виндовс_Форма
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            buttonCalculate.Click += buttonCalculate_Click;
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // Получаем значения x, y и z из TextBox'ов
+            if (double.TryParse(textBoxX.Text, out double x) &&
+                double.TryParse(textBoxY.Text, out double y) &&
+                double.TryParse(textBoxZ.Text, out double z))
+            {
+                // Вычисляем результат функции a
+                double result = CalculateA(x, y, z);
+
+                // Выводим результат в TextBox
+                textBoxResult.Text = result.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Ошибка ввода данных. Проверьте введенные значения.");
+            }
+        }
+
+        private double CalculateA(double x, double y, double z)
+        {
+            // Вычисляем результат функции a
+            double a = Math.Log(Math.Pow(y, -Math.Sqrt(Math.Abs(x))) * (x - (y / 2) + Math.Pow(Math.Sin(z), 2)) * Math.Atan(z));
+            return a;
+        }
+    }
+}
